aad6d5f4a9e4a51212e1c97ecf68cfdf3e860d5e
l-madden <lmadden1505@largy.ie>
2016-06-27 07:43:32 +0000

    Add files via upload

---
+++ country_language.py
@@ -0,0 +1,7 @@
+#Country language
+def language_of_country(country, language):
+    print("In %s they speak %s." % (country, language.title()))
+
+language_of_country("Ireland", "English")
+language_of_country("England", "English")
+language_of_country("France", "French")
