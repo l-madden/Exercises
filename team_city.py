1ecd89cdc8f5b68286450911628f3adceec7d6c3
l-madden <lmadden1505@largy.ie>
2016-06-27 07:43:15 +0000

    Add files via upload

---
+++ team_city.py
@@ -0,0 +1,7 @@
+#Team city 
+def city_team(team_name, city_name):
+    print("The %s represent %s." % (team_name, city_name))
+
+city_team("49ers", "Trail Blazers")
+city_team("Lakers", "LA")
+city_team("Trail Blazers","Portland")
