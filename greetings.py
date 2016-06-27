b7f31b61ea0dcceeb8c31adca70572ad0c90806e
l-madden <lmadden1505@largy.ie>
2016-06-27 09:32:56 +0200

    Delete greetings.py

--- greetings.py
+++
@@ -1,9 +0,0 @@
-def persons_name(name):
-
-    print("Hello, %s!" % name.title())
-    print("How are you, %s?" % name.title())
-    print("It's great to see you again, %s!\n" % name.title())
-
-list_of_names = ["Tarun", "Vikul", "Leonardo"]
-for name in list_of_names:
-    persons_name(name)
