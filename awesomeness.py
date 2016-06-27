eb644656317b71662abd6c6749a9be90edec625e
l-madden <lmadden1505@largy.ie>
2016-06-27 07:46:04 +0000

    Add files via upload

---
+++ awesomeness.py
@@ -0,0 +1,8 @@
+#Awesomeness
+
+names = ["Vikul","Tarun","Leonardo","Louis","Francisca"]
+
+awesome= [(names[num] + " is awesome") for num in range(0,len(names))]
+
+for name in awesome:
+    print(name)
